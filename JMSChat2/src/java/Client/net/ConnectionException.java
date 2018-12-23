e1a61f96e961cd97aab2a1fbe722f18f2765c6d9
FelixsGit <28272254+FelixsGit@users.noreply.github.com>
2018-12-23 17:14:16 +0100

    added comment

--- JMSChat2/src/java/Client/net/ConnectionException.java
+++ JMSChat2/src/java/Client/net/ConnectionException.java
@@ -2,6 +2,10 @@ package client.net;
 
 public class ConnectionException extends Exception{
     
+    /**
+     * Exception that is thrown to get more control, rather than to use JMSException
+     * @param reason 
+     */
     public ConnectionException(String reason){
         super(reason);
     }
